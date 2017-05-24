514e05a495e864e707f81f109c45e76752550ea2
Andrew Quinn <andrew.quinn2@gmail.com>
2017-05-24 00:12:03 -0400

    add google analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,8 @@
 //= require chartkick
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener 'turbolinks:load', (event) ->
+  if typeof ga is 'function'
+    ga('set', 'location', event.data.url)
+    ga('send', 'pageview')
