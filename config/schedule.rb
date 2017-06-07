aba74b4219823f27bb2c9e78628e0a8a1f371d85
Andrew Quinn <andrew.quinn2@gmail.com>
2017-06-07 16:55:39 -0400

    set path for whenever to fix cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,5 @@ set :bundle_command, "/home/deploy/.rbenv/shims/bundle exec"
 
 # update project list everyday at 2am
 every :day, :at => '2:00am' do
-  rbenv_rake "db:update"
+  rake "db:update"
 end
