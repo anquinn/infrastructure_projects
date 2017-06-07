0425b87647a19fbf9cda79260b063066b4598c7f
Andrew Quinn <andrew.quinn2@gmail.com>
2017-06-07 15:30:00 -0400

    attempted fix of cronjob

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,11 @@
+job_type :rbenv_rake, %Q{export PATH=/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
+                         cd :path && :environment_variable=:environment :bundle_command rake :task --silent :output }
+
 # update project list everyday at 2am
-every :day, :at => '2:00am' do
-  rake "db:update"
+# every :day, :at => '2:00am' do
+#   rbenv_rake "db:update"
+# end
+
+every 5.minutes + 10.seconds do
+  rbenv_rake "db:update"
 end
